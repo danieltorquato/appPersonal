c5e79718b02d22969c08ad34297117c146aed564
Daniel Monteiro da Silva Torquato <danieltorquato2009@gmail.com>
2022-06-18 12:50:01 -0300

    att 1806

--- pages/addExercise/index.js
+++ pages/addExercise/index.js
@@ -1,49 +1,72 @@
 import React, { useState } from 'react';
-import { Container, Img, InputDescription, InputImage, InputImageText, InputName} from './style';
+import { Container, Img, InputDescription, InputImage, InputImageText, InputName, InputUpload} from './style';
 import firebase from "../../src/services/firebaseConnection"
-import Icon from 'react-native-vector-icons/Feather';
 import * as ImagePicker from 'expo-image-picker';
-import { ref } from 'firebase/database';
 import { Alert } from 'react-native';
 export default function addExercise () {
-  const [image, setImage] = useState(null);
-
+  const [images, setImage] = useState(null);
+  const [names, setName] = useState('');
+  const [descriptions, setDescription] = useState('');
+  
  const  pickImage = async () => {
     // No permissions request is necessary for launching the image library
-    let result = await ImagePicker.launchImageLibraryAsync({
+    const result = await ImagePicker.launchImageLibraryAsync({
       mediaTypes: ImagePicker.MediaTypeOptions.All,
       allowsEditing: true,
       aspect: [4, 3],
       quality: 1,
     });
+  
 console.log(result)
     if (!result.cancelled) {
       setImage(result.uri);
-      uploadImage(result.uri, "test-image2")
-      .then(()=>{
-        Alert.alert('Sucesso')
-      }).catch((error)=>{
-        Alert.alert(error)
-      })
+     // uploadImage(result.uri, "test-image2")
+      
     }
  
   };
  const uploadImage = async (uri, imageName)=> {
+  uri = result.uri
+  imageName = names
 const response = await fetch(uri);
 const blob = await response.blob();
 var storage = firebase.storage();
 var storageRef = storage.ref().child("images/"+imageName);
-return storageRef.put(blob)
+return storageRef.put(blob).then(()=>{
+    Alert.alert('Sucesso')
+  }).catch((error)=>{
+    Alert.alert(error)
+  })
   }
 
+
+  async function addExercise( name, description, image){
+  image = images;
+  name = names;
+  description = descriptions
+    await firebase.database().ref('Exercise').child(name).set({
+     
+      name: name,
+      image: image,
+      description: description
+    })
+    alert('Pegando')
+    
+  }
   return (
   
   <Container>
     
-    <InputName placeholder="Insira o exercício" ></InputName>
-    <InputDescription placeholder="Insira a descrição"></InputDescription>
-    <InputImage onPress={pickImage}><Img source={{uri:image?image:"https://www.pngall.com/wp-content/uploads/12/Avatar-PNG-Image.png"}}/></InputImage>
-
+    <InputName placeholder="Insira o exercício" onChangeText={text=>{
+      setName(text)
+    }}></InputName>
+    <InputImageText>{names}</InputImageText>
+    <InputDescription placeholder="Insira a descrição" onChangeText={text=>{
+      setDescription(text)
+    }}></InputDescription>
+    <InputImageText>{descriptions}</InputImageText>
+    <InputImage onPress={pickImage}><Img source={{uri:images?images:"https://www.pngall.com/wp-content/uploads/12/Avatar-PNG-Image.png"}}/></InputImage>
+    <InputUpload onPress={addExercise}><InputImageText>Alora</InputImageText></InputUpload>
    
     </Container>
     )
